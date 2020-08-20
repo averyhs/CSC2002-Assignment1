e309943ef756a190eaefe275ab72fa4d453ec934
click-to-generate-name <k4s.txt@gmail.com>
2020-08-20 10:43:26 +0200

    Made ElevationAnalysis.clearFlags() static
    clearFlags() only operates on the static variable map, so makes sense to be static.

--- src/ElevationAnalysis.java
+++ src/ElevationAnalysis.java
@@ -203,7 +203,7 @@ public class ElevationAnalysis extends RecursiveTask<Integer> {
 	/**
 	 * <p>Clears flags on all points in the map.</p> 
 	 */
-	public void clearFlags() {
+	public static void clearFlags() {
 		for (int i=0; i<map.length; i++) {
 			for (int j=0; j<map[0].length; j++) {
 				map[i][j].clearFlag();
